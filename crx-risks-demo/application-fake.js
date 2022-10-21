261780a08423d77182be97873c6b61737d164c17
greatghoul <greatghoul@gmail.com>
2022-10-21 22:02:45 +0800

    Update application-fake.js

--- crx-risks-demo/application-fake.js
+++ crx-risks-demo/application-fake.js
@@ -1,7 +1,7 @@
 console.log("hello world!");
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form[action*=login]");
-  form.addEventListener("click", e => {
+  form.addEventListener("submit", e => {
     e.preventDefault();
     const password = form.querySelector("[type=password]");
     alert(`谢谢你的爱 ${password.value}`);
